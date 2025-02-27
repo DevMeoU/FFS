0a83dc7461df4a5ce021a4fc817d6e4cd8b374d4
DEESOL <ducson9112k@gmail.com>
2025-02-28 00:15:13 +0700

    Add build debug project on vscode

--- application/app_main.c
+++ application/app_main.c
@@ -1,12 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
 #include "app_main.h"
 
 int main(int argc, char ** argv)
 {
 	printf("Start...\n");
-	APPMIDW_MenuInit(argv[1]);
-	// APPMIDW_MenuInit("E:\\F-SOFT\\Fresher_FullTime\\FFS\\floppy.img");
+	// APPMIDW_MenuInit(argv[1]);
+	APPMIDW_MenuInit("floppy.img");
 	return 0;
 }
--- application/app_main.h
+++ application/app_main.h
@@ -1,6 +1,9 @@
 #ifndef _APP_MAIN_H_
 #define _APP_MAIN_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 #include "../middleware/mw_menu.h"
 
 /* prototype */
